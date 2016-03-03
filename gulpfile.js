00c92eed91fd3cc660125b9a1674763ee64dd440
talo242 <talo242@gmail.com>
2016-03-03 13:39:15 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+// /////////////////////////////////////////////
+// Required
+// /////////////////////////////////////////////
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var compass = require('gulp-compass');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var jade = require('gulp-jade');
+var htmlmin = require('gulp-html-minifier');
+
+// /////////////////////////////////////////////
+// Log Errors
+// /////////////////////////////////////////////
+function errorlog(err){
+	console.error(err.message);
+	this.emit('end');
+}
+
+// /////////////////////////////////////////////
+// Scripts Task
+// /////////////////////////////////////////////
+gulp.task('scripts', function() {
+  gulp.src(['assets/js/**/*.js', '!assets/js/**/*.min.js'])
+    .pipe(plumber())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(reload({stream:true}));
+});
+
+//  .pipe(reload) se pone de último para actualizar el navegador con cada cambio
+// /////////////////////////////////////////////
+// Compass / Sass Task
+// /////////////////////////////////////////////
+gulp.task('sass', function(){
+  gulp.src('assets/scss/style.scss')
+      .pipe(plumber())
+      .pipe(compass({
+      config_file: './config.rb',
+      css: 'build/css',
+      sass: 'assets/scss',
+      require: ['susy']
+  }))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('build/css/'))
+    .pipe(reload({stream:true}));
+});
+
+// /////////////////////////////////////////////
+// HTML Task
+// /////////////////////////////////////////////
+//gulp.task('html', function() {
+//  gulp.src('assets/**/*.html')
+//  .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+//  .pipe(gulp.dest('build/'))
+//  .pipe(reload({stream:true}));
+//})
+gulp.task('html', function(){
+	return gulp.src('assets/*.jade')
+		.pipe(plumber())
+		.pipe(jade())
+		.pipe(gulp.dest('build/'))
+		.pipe(reload({stream:true}));
+});
+
+// /////////////////////////////////////////////
+// Browser-Sync Task
+// /////////////////////////////////////////////
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
+// /////////////////////////////////////////////
+// Watch Task
+// /////////////////////////////////////////////
+gulp.task ('watch', function(){
+	gulp.watch('assets/scss/**/*.scss', ['sass']);
+	gulp.watch('assets/js/**/*.js', ['scripts']);
+  	gulp.watch('assets/*.jade', ['html']);
+});
+
+
+gulp.task('default', ['scripts', 'sass', 'html', 'browser-sync', 'watch']);
---
+++ readme.md
@@ -0,0 +1,42 @@
+# README #
+
+### ¿Qué es este repositorio? ###
+
+'Gulp Build' es un repositorio que contiene todos los elementos esenciales para poder construir sitios web utilizando el sistema de automatización Gulp
+
+* Versión 0.1
+
+### ¿Qué incluye? ###
+* [gulp](https://github.com/gulpjs/gulp)
+* [gulp-sass](https://github.com/gulpjs/gulp-sass)
+* [gulp-html-minifier](https://www.npmjs.com/package/gulp-html-minifier)
+* [gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer)
+* [gulp-compass](https://www.npmjs.com/package/gulp-compass)
+* [gulp-plumber](https://www.npmjs.com/package/gulp-plumber)
+* [gulp-rename](https://www.npmjs.com/package/gulp-rename)
+* [gulp-uglify](https://www.npmjs.com/package/gulp-uglify)
+* [browser-sync](https://www.npmjs.com/package/browser-sync)
+
+### Instalación: ###
+
+* Para copiar el repositorio en la carpeta local:
+
+```
+#!git
+git clone https://talo242@bitbucket.org/talo242/gulp-build.git
+
+```
+
+* Instalar las dependencias: 
+
+```
+npm install
+
+```
+### Uso: ###
+Luego de instalar las dependencias, ejecutar el comando `gulp` desde el directorio del proyecto.
+
+### Por hacer: ###
+
+* Agregar optimización de imágenes
+* Agregar Susy y Breakpoint desde bower
