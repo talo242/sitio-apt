fd1216222fc8cf241d2fe0ca29afcf78e746998e
talo242 <talo242@gmail.com>
2016-03-15 22:49:14 -0600

    Terminado el home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,48 @@
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('header').outerHeight();
-// var tempScrollTop
-//
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
-//
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//         tempScrollTop = $(window).scrollTop();
-//     }
-// }, 250);
-//
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
-//
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
-//
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('header').removeClass('nav-down').addClass('nav-up');
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('header').removeClass('nav-up').addClass('nav-down');
-//         }
-//     }
-//
-//     lastScrollTop = st;
-// }
 $("document").ready(function() {
+  // Ocultar barra al dar scroll
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.navbar').outerHeight();
+  var tempScrollTop
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+      tempScrollTop = $(window).scrollTop();
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+      // Scroll Down
+      $('.navbar').removeClass('nav-down').addClass('nav-up');
+      $(".nav-first").removeClass("nav-first-active");
+      $(".nav-second").removeClass("nav-second-active");
+    } else {
+      // Scroll Up
+      if(st + $(window).height() < $(document).height()) {
+        $('.navbar').removeClass('nav-up').addClass('nav-down');
+      }
+    }
+
+    lastScrollTop = st;
+  }
+
+
   var visible = false;
   // Ocultar los submenús
   // $(".nav-second").hide();
